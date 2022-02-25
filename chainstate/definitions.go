63fc58c7b11741601616d6d8f4e7884afc3379b2
mrz1836 <mrz1818@gmail.com>
2022-02-25 08:35:47 -0500

    Bump version

--- chainstate/definitions.go
+++ chainstate/definitions.go
@@ -7,7 +7,7 @@ const (
 	defaultBroadcastTimeOut      = 15 * time.Second
 	defaultQueryTimeOut          = 15 * time.Second
 	defaultUserAgent             = "go-chainstate: " + version
-	version                      = "v0.1.3"
+	version                      = "v0.1.4"
 	whatsOnChainRateLimitWithKey = 20
 )
 

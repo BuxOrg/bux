9ade90539a09ea6b23026f77990efd109ea894e5
mrz1836 <mrz1818@gmail.com>
2022-03-07 20:39:25 -0500

    Added example of loading the paymail model

--- examples/client/paymail_model/paymail_model.go
+++ examples/client/paymail_model/paymail_model.go
@@ -1,1 +1,26 @@
-package paymail_model
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/BuxOrg/bux"
+	"github.com/BuxOrg/bux/cachestore"
+	"github.com/BuxOrg/bux/taskmanager"
+)
+
+func main() {
+	client, err := bux.NewClient(
+		context.Background(), // Set context
+		bux.WithRistretto(cachestore.DefaultRistrettoConfig()),                                 // Cache
+		bux.WithTaskQ(taskmanager.DefaultTaskQConfig("test_queue"), taskmanager.FactoryMemory), // Tasks
+		bux.WithAutoMigrate(&bux.PaymailAddress{
+			Model: *bux.NewBaseModel(bux.ModelPaymail), // Add the paymail model (for use in BUX server & clients)
+		}),
+	)
+	if err != nil {
+		log.Fatalln("error: " + err.Error())
+	}
+
+	log.Println("client loaded!", client.UserAgent())
+}

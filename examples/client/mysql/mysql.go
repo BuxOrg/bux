cebcd96b16c20374df4ee6b12e12cefed3d4e77b
mrz1836 <mrz1818@gmail.com>
2022-03-31 12:16:59 -0400

    Added an example for MySQL

---
+++ examples/client/mysql/mysql.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	"github.com/BuxOrg/bux"
+	"github.com/BuxOrg/bux/cachestore"
+	"github.com/BuxOrg/bux/datastore"
+	"github.com/BuxOrg/bux/taskmanager"
+)
+
+func main() {
+	defaultTimeouts := 10 * time.Second
+
+	client, err := bux.NewClient(
+		context.Background(), // Set context
+		bux.WithSQL(datastore.MySQL, &datastore.SQLConfig{ // Load using a MySQL configuration
+			CommonConfig: datastore.CommonConfig{
+				Debug:                 true,
+				MaxConnectionIdleTime: defaultTimeouts,
+				MaxConnectionTime:     defaultTimeouts,
+				MaxIdleConnections:    10,
+				MaxOpenConnections:    10,
+				TablePrefix:           "bux",
+			},
+			Driver:    datastore.MySQL.String(),
+			Host:      "localhost",
+			Name:      os.Getenv("DB_NAME"),
+			Password:  os.Getenv("DB_PASSWORD"),
+			Port:      "3306",
+			TimeZone:  "UTC",
+			TxTimeout: defaultTimeouts,
+			User:      os.Getenv("DB_USER"),
+		}),
+		bux.WithRistretto(cachestore.DefaultRistrettoConfig()),                                 // Cache
+		bux.WithTaskQ(taskmanager.DefaultTaskQConfig("test_queue"), taskmanager.FactoryMemory), // Tasks
+		bux.WithAutoMigrate( // All models
+			append(bux.BaseModels, &bux.PaymailAddress{
+				Model: *bux.NewBaseModel(bux.ModelPaymail), // Add the paymail model (for use in BUX server & clients)
+			})...,
+		),
+	)
+	if err != nil {
+		log.Fatalln("error: " + err.Error())
+	}
+
+	log.Println("client loaded!", client.UserAgent())
+}

cadb8871c7e2f7b6b48206a27d255f4ea169c3e5
Siggi <github@icellan.net>
2022-04-14 14:08:55 +0200

    Inititial support for tokens (STAS)

--- utils/errors.go
+++ utils/errors.go
@@ -15,3 +15,6 @@ var ErrHDKeyNil = errors.New("hd key is nil")
 
 // ErrDeriveFailed is when the address derivation failed
 var ErrDeriveFailed = errors.New("derive addresses failed, missing addresses")
+
+// ErrCouldNotDetermineDestinationOutput error when token output could not be determined
+var ErrCouldNotDetermineDestinationOutput = errors.New("could not determine token output destination")

62c7625ba54e0bbeb8ab8c5bae0bcb153adf9ba1
Siggi <github@icellan.net>
2022-04-21 14:15:34 +0200

    Added marshal/unmarshal for graphql for QueryParams

--- datastore/params.go
+++ datastore/params.go
@@ -1,9 +1,37 @@
 package datastore
 
+import (
+	"encoding/json"
+
+	"github.com/99designs/gqlgen/graphql"
+)
+
 // QueryParams object to use when limiting and sorting database query results
 type QueryParams struct {
-	Page          int    `json:"page"`
-	PageSize      int    `json:"page_size"`
-	OrderByField  string `json:"order_by_field"`
-	SortDirection string `json:"sort_direction"`
+	Page          int    `json:"page,omitempty"`
+	PageSize      int    `json:"page_size,omitempty"`
+	OrderByField  string `json:"order_by_field,omitempty"`
+	SortDirection string `json:"sort_direction,omitempty"`
+}
+
+// MarshalQueryParams will marshal the custom type
+func MarshalQueryParams(m QueryParams) graphql.Marshaler {
+	if m.Page == 0 && m.PageSize == 0 && m.OrderByField == "" && m.SortDirection == "" {
+		return graphql.Null
+	}
+	return graphql.MarshalAny(m)
+}
+
+// UnmarshalQueryParams will unmarshal the custom type
+func UnmarshalQueryParams(v interface{}) (QueryParams, error) {
+	if v == nil {
+		return QueryParams{}, nil
+	}
+
+	var q QueryParams
+	if err := json.Unmarshal([]byte(v.(string)), &q); err != nil {
+		return QueryParams{}, err
+	}
+
+	return q, nil
 }

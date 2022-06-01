bb9c6fff2e8a41239a8557b0c495061b39ff6281
galt-tr <dylan@truereviews.io>
2022-06-01 15:59:49 -0400

    If bloom filter is explicitly set don't panic

--- chainstate/monitor.go
+++ chainstate/monitor.go
@@ -84,7 +84,6 @@ func NewMonitor(_ context.Context, options *MonitorOptions) (monitor *Monitor) {
 	switch monitor.filterType {
 	case FilterRegex:
 		monitor.processor = NewRegexProcessor()
-	case FilterBloom:
 	default:
 		monitor.processor = NewBloomProcessor(uint(monitor.maxNumberOfDestinations), monitor.falsePositiveRate)
 	}

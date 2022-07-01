6d83416c45e2e7cb687255bbc2363be8c669d7bd
mrz1836 <mrz1818@gmail.com>
2022-07-01 12:28:38 -0400

    Bumped version

--- definitions.go
+++ definitions.go
@@ -23,7 +23,7 @@ const (
 	dustLimit                      = uint64(546)       // Dust limit
 	mongoTestVersion               = "4.2.1"           // Mongo Testing Version
 	sqliteTestVersion              = "3.37.0"          // SQLite Testing Version (dummy version for now)
-	version                        = "v0.2.32"         // bux version
+	version                        = "v0.2.33"         // bux version
 )
 
 // Defaults for task cron jobs (tasks)

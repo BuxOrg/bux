a4705b73ffcd8beb0662c291217302facc235773
vitalibalashka <vitalybalashko@gmail.com>
2023-09-14 08:16:15 +0200

    feat: remove option to use whatsonchain and nownode for broadcasting

--- chainstate/broadcast.go
+++ chainstate/broadcast.go
@@ -101,16 +101,6 @@ func createActiveProviders(c *Client, txID, txHex string) []txBroadcastProvider 
 		}
 	}
 
-	if shouldBroadcastToWhatsOnChain(c) {
-		pvdr := whatsOnChainBroadcastProvider{txID: txID, txHex: txHex}
-		providers = append(providers, &pvdr)
-	}
-
-	if shouldBroadcastToNowNodes(c) {
-		pvdr := nowNodesBroadcastProvider{uniqueID: txID, txID: txID, txHex: txHex}
-		providers = append(providers, &pvdr)
-	}
-
 	if shouldBroadcastWithBroadcastClient(c) {
 		pvdr := broadcastClientProvider{txID: txID, txHex: txHex}
 		providers = append(providers, &pvdr)
@@ -122,15 +112,6 @@ func createActiveProviders(c *Client, txID, txHex string) []txBroadcastProvider 
 func shouldBroadcastWithMAPI(c *Client) bool {
 	return !utils.StringInSlice(ProviderMAPI, c.options.config.excludedProviders) &&
 		(c.Network() == MainNet || c.Network() == TestNet) // Only supported on main and test right now
-}
-
-func shouldBroadcastToWhatsOnChain(c *Client) bool {
-	return !utils.StringInSlice(ProviderWhatsOnChain, c.options.config.excludedProviders)
-}
-
-func shouldBroadcastToNowNodes(c *Client) bool {
-	return !utils.StringInSlice(ProviderNowNodes, c.options.config.excludedProviders) &&
-		c.NowNodes() != nil // Only if NowNodes is loaded (requires API key)
 }
 
 func shouldBroadcastWithBroadcastClient(c *Client) bool {

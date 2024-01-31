1f914f8184c4cbadf434651e13098a6a86b28244
wregulski <wojciechregulski@outlook.com>
2024-01-31 10:42:42 +0100

    feat: rebase fixtures

--- examples/client/custom_cron/custom_cron.go
+++ examples/client/custom_cron/custom_cron.go
@@ -11,8 +11,8 @@ import (
 func main() {
 	client, err := bux.NewClient(
 		context.Background(), // Set context
-		bux.WithCronCustmPeriod(bux.CronJobNameDraftTransactionCleanUp, 2*time.Second),
-		bux.WithCronCustmPeriod(bux.CronJobNameSyncTransactionSync, 4*time.Second),
+		bux.WithCronCustomPeriod(bux.CronJobNameDraftTransactionCleanUp, 2*time.Second),
+		bux.WithCronCustomPeriod(bux.CronJobNameSyncTransactionSync, 4*time.Second),
 	)
 	if err != nil {
 		log.Fatalln("error: " + err.Error())

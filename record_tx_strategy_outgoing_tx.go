058ff37ca84f568a52e6c7bb5dd9c51c994b83ea
Nazarii-4chain <nazarii.hrohol@4chain.studio>
2024-02-01 16:15:05 +0200

    Added validation

--- record_tx_strategy_outgoing_tx.go
+++ record_tx_strategy_outgoing_tx.go
@@ -70,6 +70,10 @@ func (strategy *outgoingTx) Validate() error {
 		return ErrMissingFieldHex
 	}
 
+	if _, err := bt.NewTxFromString(strategy.Hex); err != nil {
+		return fmt.Errorf("invalid hex: %w", err)
+	}
+
 	if strategy.RelatedDraftID == "" {
 		return errors.New("empty RelatedDraftID")
 	}
@@ -82,12 +86,7 @@ func (strategy *outgoingTx) Validate() error {
 }
 
 func (strategy *outgoingTx) TxID() string {
-	btTx, err := bt.NewTxFromString(strategy.Hex)
-
-	// Return hex if hex is invalid. Handle error in error handlers
-	if err != nil {
-		return strategy.Hex
-	}
+	btTx, _ := bt.NewTxFromString(strategy.Hex)
 	return btTx.TxID()
 }
 

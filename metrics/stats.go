0c9e6b709ee040ad1f313b250de7fc0e13542374
Krzysztof Tomecki <152964795+chris-4chain@users.noreply.github.com>
2024-02-12 07:03:31 +0100

    feat(BUX-567): collecting stats in a cronJob

--- metrics/stats.go
+++ metrics/stats.go
@@ -2,11 +2,23 @@ package metrics
 
 // Stats is a struct that contains all the gauges that are used to track the calculated stats of the application
 type Stats struct {
-	XPub GaugeInterface
+	XPub           GaugeInterface
+	Utxo           GaugeInterface
+	TransactionIn  GaugeInterface
+	TransactionOut GaugeInterface
+	Paymail        GaugeInterface
+	Destination    GaugeInterface
+	AccessKey      GaugeInterface
 }
 
 func registerStats(collector Collector) Stats {
 	return Stats{
-		XPub: collector.RegisterGauge(xpubGaugeName),
+		XPub:           collector.RegisterGauge(xpubGaugeName),
+		Utxo:           collector.RegisterGauge(utxoGaugeName),
+		TransactionIn:  collector.RegisterGauge(transactionInGaugeName),
+		TransactionOut: collector.RegisterGauge(transactionOutGaugeName),
+		Paymail:        collector.RegisterGauge(paymailGaugeName),
+		Destination:    collector.RegisterGauge(destinationGaugeName),
+		AccessKey:      collector.RegisterGauge(accessKeyGaugeName),
 	}
 }
